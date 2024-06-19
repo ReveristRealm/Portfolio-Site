3d44225a1183e9c4c8b571afac7940d56858b724
Daniel Jackson <iamdanielj@outlook.com>
2024-06-18 22:19:35 -0400

    Working on the About-Me

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,23 @@
+import Networking from "../Networking/Networking.jsx";
 import Button from "./Button.jsx";
 import "./Header.css";
 
 export default function Header() {
   return (
-    <header>
-      <ul>
-        <Button>About Me</Button>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Networking</li>
-        <li>Extra</li>
-      </ul>
-    </header>
+    <>
+      <header>
+        <ul>
+          <Button>About Me</Button>
+          <Button>Skills</Button>
+          <Button>Projects</Button>
+          <Button>Networking</Button>
+          <Button>Extra</Button>
+        </ul>
+      </header>
+
+      <div className="container">
+        <Networking></Networking>
+      </div>
+    </>
   );
 }
---
+++ src/Components/Networking/Networking.jsx
@@ -0,0 +1,11 @@
+import "./Networking.css";
+
+export default function Networking() {
+  return (
+    <>
+      <div className="intro">
+        <h1>Welcome</h1>
+      </div>
+    </>
+  );
+}
